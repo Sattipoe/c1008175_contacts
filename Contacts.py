45df516122f0aba3f36e292cb87de02621ed9fd4
Samuel Attipoe <c1008175@newcastle.ac.uk>
2021-11-13 15:28:12 +0000

    added ui and way to write to txt file

--- Contacts.py
+++ Contacts.py
@@ -7,15 +7,43 @@ class Contacts:
         self.address = address
         self.birthday = birthday
         self.telephone = telephone
-class contact_book:
-    def __init__(self):
-        contactbook = []
-        with open('contacts.txt') as f:
-            lines = f.readlines()
-        self.contactbook.append(Contacts(name,(Contacts)address, birthday, telephone ))
 
 
-if __name__ == "__main__":
+
+f = open('contacts.txt', 'r')
+contents = f.read().split('\n')
+f.close()
+
+##print(contents[1])
+    user_input = ""
+
+
+    print ("press 1 to show all contacts")
+    while user_input != "e":
+        print ("options = ")
+        print("1 = enter contact information")
+        print ("2 = display all contacts")
+        print ("e = close program ")
+        user_input = input("select option: ")
+        if user_input == "1":
+            print("enter contact information")
+
+
+            name = input( " First and last name = ")
+            address = input("Address =  ")
+            birthday = input(" Birthday = ")
+            telephone = input(" Telephone number = ")
+
+            person = [name, ", ", address, ",", birthday, ", ", telephone]
+            with open('contacts.txt', 'a') as f:
+            for persons in person:
+            f.write(persons)
+            f.write('\n')
+
+            print ("details have been stored ")
+
+
+##if __name__ == "__main__"
     ##contactList =["Samuel Attipoe", "19" , "E111 tweed castle leazes", "28/08/2002", "07716548718" ]
     #contact1 = contacts("bill", "17", "sddress", "01/02/0304", "0192837465")
     #contactList=[]
