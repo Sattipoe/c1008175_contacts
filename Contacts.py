9c11f7518fa81ecffdf83d6ad531500e767c6bb2
Samuel Attipoe <c1008175@newcastle.ac.uk>
2021-11-18 13:59:29 +0000

    added more comments and .lower for  case sensitivity issue

--- Contacts.py
+++ Contacts.py
@@ -1,4 +1,6 @@
-from lib2to3.pgen2.grammar import name
+import sys
+import sysconfig
+
 
 
 class Contacts:
@@ -14,17 +16,18 @@ class Contacts:
 
 contact_list = list()
 
-
-
+# opens the contact file and reads it
 f = open('contacts.txt', 'r')
-contact_list = f.readlines()
+contact_list = f.readlines()#readlines seperated it into lines
 f.close()
 
 
 
 
-##print(contents[1])
-user_input = ""
+
+user_input = ""#to be able to give the user input a variable late
+
+
 
 
 while user_input != "e":
@@ -32,8 +35,9 @@ while user_input != "e":
         print("1 = enter contact information")
         print ("2 = display all contacts")
         print ("e = close program ")
-        print("3 = search for a contact ")
-        user_input = input("select option: ")
+        print("3 = search for a contact ")#interface that shows up when code is ran
+        user_input = input("select option: ")#input allows the user to input something
+
 
 
         if user_input == "1":
@@ -43,63 +47,55 @@ while user_input != "e":
             name = input( " First and last name = ")
             address = input("Address =  ")
             birthday = input(" Birthday = ")
-            telephone = input(" Telephone number = ")
+            telephone = input(" Telephone number = ")# used to alllow user to enter contact info
 
-            person = [name, ",", address, ",", birthday, ",", telephone]
+            person = [",", name, ",", address, ",", birthday, ",", telephone]
             with open('contacts.txt', 'a') as f:
                 for persons in person:
-                    f.write(persons)
-                f.write('\n')
+                    f.write(persons.lower())
+                f.write('\n')#opens the contacts text after formatting the user information that has been inputed and writes it back to the txt file
 
             print ("details have been stored ")
         elif user_input == "2":
             file = open('contacts.txt', 'r')
             data = file.read()
             file.close()
-            print(data)
+            print(data)# opens the current txt file and prints out all the information
+
         elif user_input == "3":
-            search = input(" enter the name of the contact you want to find: ")
+            search = input(" enter the name/address/number/birthday of the contact you want to find: ")
             count=-1
             for line in contact_list:
-                count+=1
-                if search in line:
-                    print(str(count) + " "+ line)
+                count+=1 #gives the contacts a number to allow differentiation
+
+                if search.lower() in line:# if the search is in the line print the entire line also lower forces it into lower case
+                    print(str(count) + " "+ line) # changes the count into a string
                     edit = input("would you like to edit the contact")
+                    delete = input("would you like to delete this Contact")
                     if edit == "yes":
+
                         new_input = input("Enter new details seperated by , : ")
                         f = open('contacts.txt', 'r')
                         contact_list = f.readlines()
                         f.close()
 
-                        contact_list[count] = new_input
+                        contact_list[count] = new_input #contact_list[count] is the thing holding the data that has been searched
+                        f = open('contacts.txt', 'w')
+                        for i in range(len(contact_list)):
+                            f.write(contact_list[i])
+                        f.close()
+                        delete = input("would you like to delete this Contact")
+                    if delete == "yes":
+                        f = open("contacts.txt","r")
+                        contact_list = f.readlines()
+                        f.close()
+
+                        contact_list.pop(count)
 
                         f = open('contacts.txt', 'w')
                         for i in range(len(contact_list)):
                             f.write(contact_list[i])
                         f.close()
 
-
-
 #  used code from https://www.delftstack.com/howto/python/python-replace-line-in-file/#:~:text=Use%20the%20for%20Loop%20Along%20With%20the%20replace,Replace%20the%20Text%20in%20a%20Line%20in%20Pytho
 # to help with the process of replacing and updating code
-
-
-
-
-
-
-
-
-
-
-
-##if __name__ == "__main__"
-    ##contactList =["Samuel Attipoe", "19" , "E111 tweed castle leazes", "28/08/2002", "07716548718" ]
-    #contact1 = contacts("bill", "17", "sddress", "01/02/0304", "0192837465")
-    #contactList=[]
-    #contactList.append(Contacts("bill", "17", "sddress", "01/02/0304", "0192837465"))
-
-    #print( contactList[1].name )
-
-    #for i in range(100):
-     #   print(contactList[i].name + )
--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# c1008175_contacts
+C1008175_Contacts
+===========================
+
+How to use the program
+----------------------
+when entering names into the program be sure to enter them in lower case and search in lower case
+when asked if you would like to edit the contact please enter answer in lower case as it is case sensitive
+the same when asked if you would like to delete the contact
+
+Usage
+--------------------
+To use the program press 1 to enter information 
+then input details in the order that is asked
+after details have been entered you can press 2 to view the program
+to edit the newly entered code the program needs to be closed and re run
+
+Assumptions
+------------------
